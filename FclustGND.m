78ff1e731523e26c7a10978f60a15700a18e1eeb
Eric Fields <eric.fields@bc.edu>
2017-11-17 17:44:58 -0500

    workaround for javaaddpath problems

--- FclustGND.m
+++ FclustGND.m
@@ -134,7 +134,7 @@
 %
 %
 %AUTHOR: Eric Fields
-%VERSION DATE: 14 November 2017
+%VERSION DATE: 17 November 2017
 %
 %NOTE: This function is provided "as is" and any express or implied warranties 
 %are disclaimed. 
@@ -487,9 +487,9 @@ function [GND, results, prm_pval, F_obs, clust_info] = FclustGND(GND_or_fname, v
             fprintf('\nWriting results to %s . . . ', p.Results.output_file)
         end
         Ftest2xls(GND, length(GND.F_tests), p.Results.output_file);
-        if VERBLEVEL
-            fprintf('DONE.\n')
-        end
+%         if VERBLEVEL
+%             fprintf('DONE.\n')
+%         end
     end
     
 end
